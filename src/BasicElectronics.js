ede1f6a01f7f1d3f64b0c79a7a81d3eecd4329a9
kernelsrinivas <kernel.srinivasan@gmail.com>
2022-03-14 13:32:19 +0530

    1 file modified

--- src/BasicElectronics.js
+++ src/BasicElectronics.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//Ciruits
 const powerBasics = [
   { name: 'Current(DC/AC)' },
   { name: 'Voltage' },
@@ -33,9 +34,50 @@ const switchingComponents = [
 ];
 
 const logicComponents = [
-  { name: 'Logic Gates' },
+  { name: 'IC/Logic Gates' },
   { name: 'Flip-Flop/Latch' },
   { name: 'Counter/Timer' },
   { name: 'Interrupter/Trigger' },
   { name: 'Encoder/Decoder' },
+];
+
+//Programmings
+const controllerComponents = [
+  { name: 'Microcontroller' },
+  { name: 'Microprocessor' },
+  { name: 'FPGA/VLSI' },
+  { name: 'PLC/PAC' },
+  { name: 'DCS' },
+];
+
+const wiredCommuncation = [
+  { name: 'RS232/RS485' },
+  { name: 'SPI' },
+  { name: 'I2C' },
+  { name: 'USB' },
+  { name: 'Ethernet' },
+];
+
+const wirelessCommuncation = [
+  { name: 'RF/LoRa' },
+  { name: 'Bluetooth' },
+  { name: 'Zigbee' },
+  { name: 'WiFi' },
+  { name: 'GSM/3G/4G/5G' },
+];
+
+const fieldInputs = [
+  { name: 'Field Instruments(FLPT)' },
+  { name: 'Vision Sensor(IR/Photodiaode/Camera)' },
+  { name: 'Sonic Sensor (Ultra/Mic)' },
+  { name: 'Touch Sensor(Flex/Keypad)' },
+  { name: 'Advance Sensor(GPS/Compass/Acc/Encoder' },
+];
+
+const fieldOutputs = [
+  { name: 'Lights' },
+  { name: 'Sounds' },
+  { name: 'Motor/Eletromegnetic' },
+  { name: 'Pnematics' },
+  { name: 'Hydraulic' },
 ];
