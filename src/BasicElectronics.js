c2ed021455a38b04e8917ac0e7e4f053ea991d0b
kernelsrinivas <kernel.srinivasan@gmail.com>
2022-03-14 11:27:10 +0530

    1 file added for electronics

---
+++ src/BasicElectronics.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const basicComponents = [
+  { name: 'Diode' },
+  { name: 'Resistor' },
+  { name: 'Capacitor' },
+  { name: 'Inductor' },
+  { name: 'Transistor' },
+];
+
+const powerElement = [
+  { name: 'Current(DC/AC)' },
+  { name: 'Voltage' },
+  { name: 'Power/Load' },
+  { name: 'Transformer' },
+];
+
+const powerComponent = [
+  { name: 'Convertor' },
+  { name: 'Inverter' },
+  { name: 'Regulator' },
+  { name: 'Braker' },
+];
+
+const switchingComponent = [
+  {
+    name: 'Switch',
+  },
+  {
+    name: 'OpAmp/Comparator',
+  },
+  {
+    name: 'Optocoupler',
+  },
+  {
+    name: 'FET/MOSFET',
+  },
+  {
+    name: 'IGBT',
+  },
+];
